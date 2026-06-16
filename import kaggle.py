16e9ef59a8cfe282ee3bec4034f5f6459663be6d
alexrhys <arhys1@wgu.edu>
2026-06-16 15:28:30 -0400

    rename unzip and read genres csv and print head

--- import kaggle.py
+++ import kaggle.py
@@ -1,5 +1,12 @@
 import zipfile, pandas as pd
 
-   with zipfile.ZipFile('steam-games-dataset-2025.zip', 'r') as z:
-       z.extractall('data/')
+with zipfile.ZipFile('steam-games-dataset-2025.zip', 'r') as z:
+    print('\nShow files in ZIP')
+    print(z.namelist())  # shows all files in the zip
+    z.extractall('.')
 
+# open genres as df
+df = pd.read_csv('steam_dataset_2025_csv/genres.csv')
+
+print('\nprint genre head')
+print(df.head())
