7bfccf2c25ae2af3c7d8d8df9c13cc1ce114c8a1
alexrhys <arhys1@wgu.edu>
2026-06-16 15:37:57 -0400

    create dfs for reviews and applications

--- import kaggle.py
+++ import kaggle.py
@@ -5,8 +5,20 @@ with zipfile.ZipFile('steam-games-dataset-2025.zip', 'r') as z:
     print(z.namelist())  # shows all files in the zip
     z.extractall('.')
 
+# open applications as df
+df_applications = pd.read_csv('steam_dataset_2025_csv/applications.csv')
+
+print('\nprint applications head')
+print(df_applications.head())
+
 # open genres as df
-df = pd.read_csv('steam_dataset_2025_csv/genres.csv')
+df_genre = pd.read_csv('steam_dataset_2025_csv/genres.csv')
 
 print('\nprint genre head')
-print(df.head())
+print(df_genre.head())
+
+# open reviews as df
+df_reviews = pd.read_csv('steam_dataset_2025_csv/reviews.csv')
+
+print('\nprint reviews head')
+print(df_reviews.head())
