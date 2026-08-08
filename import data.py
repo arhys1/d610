f8f7e3a90ac4bebe7d743daff714c0ccda14ef85
alexrhys <arhys1@wgu.edu>
2026-08-08 16:50:22 -0400

    reframe import data.py as defining a function to call in main.py

--- import data.py
+++ import data.py
@@ -5,39 +5,46 @@
 
 import zipfile, pandas as pd
 
-with zipfile.ZipFile('steam-games-dataset-2025.zip', 'r') as z:
+def load_steam_data():
+    with zipfile.ZipFile('steam-games-dataset-2025.zip', 'r') as z:
+        z.extractall('.')
+
+    # open applications as df
+    df_applications = pd.read_csv('steam_dataset_2025_csv/applications.csv', low_memory=False)
+    # open genres as df
+    df_genre = pd.read_csv('steam_dataset_2025_csv/genres.csv')
+    # open reviews as df
+    df_reviews = pd.read_csv('steam_dataset_2025_csv/reviews.csv')
+
+    return df_applications, df_genre, df_reviews
+
+## test load_steam_data() function and print df info
+if __name__ == "__main__":
+    df_applications, df_genre, df_reviews = load_steam_data()
+
     print('\nSHOW FILES IN ZIP')
-    print(z.namelist())  # shows all files in the zip
-    z.extractall('.')
+    with zipfile.ZipFile('steam-games-dataset-2025.zip', 'r') as z:
+        print(z.namelist())
 
-# open applications as df
-df_applications = pd.read_csv('steam_dataset_2025_csv/applications.csv', low_memory=False)
+    print('\nPRINT APPLICATIONS INFO')
+    print(df_applications.info())
 
-print('\nPRINT APPLICATIONS INFO')
-print(df_applications.info())
+    print('\nPRINT APPLICATIONS HEAD')
+    print(df_applications.head())
 
-print('\nPRINT APPLICATIONS HEAD')
-print(df_applications.head())
+    print('\nPRINT GENRE INFO')
+    print(df_genre.info())
 
-# open genres as df
-df_genre = pd.read_csv('steam_dataset_2025_csv/genres.csv')
+    print('\nPRINT GENRE HEAD')
+    print(df_genre.head())
 
-print('\nPRINT GENRE INFO')
-print(df_genre.info())
+    print('\nLIST ALL GENRES')
+    print(sorted(df_genre['name'].unique())) ## list each genre in the df
+    print(df_genre['name'].nunique())
 
-print('\nPRINT GENRE HEAD')
-print(df_genre.head())
+    print('\nPRINT REVIEWS INFO')
+    print(df_reviews.info()) ## note: we will use "voted_up" as the basis for a positive review
 
-print('\nLIST ALL GENRES')
-print(sorted(df_genre['name'].unique())) ## list each genre in the df
-print(df_genre['name'].nunique())
-
-# open reviews as df
-df_reviews = pd.read_csv('steam_dataset_2025_csv/reviews.csv')
-
-print('\nPRINT REVIEWS INFO')
-print(df_reviews.info()) ## note: we will use "voted_up" as the basis for a positive review
-
-print('\nPRINT REVIEWS HEAD')
-print(df_reviews.head())
+    print('\nPRINT REVIEWS HEAD')
+    print(df_reviews.head())
 
